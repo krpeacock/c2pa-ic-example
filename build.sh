db15b131ed462c8f28188f802ec05f0c9fe83962
Kyle Peacock <kylpeacock@gmail.com>
2023-08-04 16:03:13 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+
+CANISTER="c2pa_backend"
+TARGET="wasm32-unknown-unknown"
+SRC_DIR="$PWD/src/$CANISTER"
+cargo_build_args=(
+    --manifest-path "$SRC_DIR/Cargo.toml"
+    --target "$TARGET"
+    --release
+    -j1
+)
+echo "Building $CANISTER"
+cargo build "${cargo_build_args[@]}"
+cp target/wasm32-unknown-unknown/release/c2pa_backend.wasm ./c2pa_backend.wasm
+echo "Optimizing $CANISTER"
+ic-wasm ./c2pa_backend.wasm shrink
+gzip c2pa_backend.wasm 
+echo "Installing $CANISTER"
+dfx canister install c2pa_backend --wasm ./c2pa_backend.wasm.gz
--- src/c2pa_backend/src/lib.rs
+++ src/c2pa_backend/src/lib.rs
@@ -1,4 +1,26 @@
+use c2pa::ManifestStore;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query]
+fn check_manifest() -> String {
+    // load ./profile-authenticated.jpeg as bytes
+    let maybe_file = std::fs::read("../foo.jpeg");
+    if maybe_file.is_err() {
+        return format!("Error reading file");
+    }
+
+    // convert result to &[u8]
+    let maybe_file = maybe_file.unwrap();
+
+    let ms = ManifestStore::from_bytes("image/jpeg", &maybe_file, true);
+
+    if let Ok(ms) = ms {
+        return format!("{}", ms);
+    } else {
+        return format!("Error");
+    }
 }
